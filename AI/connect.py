d707fad90ed69412ebe9228528e5ab656c4e27c7
Dhiraj Karangale <dakarangale02@gmail.com>
2025-11-12 18:20:53 +0530

    Lazy Load Added for LLM Connections

--- AI/connect.py
+++ AI/connect.py
@@ -8,7 +8,8 @@ load_dotenv()
 with open("LLMs.json", "r") as file:
     LLMs = json.load(file)
 
-_connected_llms = None
+# _connected_llms = None
+_connected_llms = {}
 
 
 def connect_llms():
@@ -45,12 +46,42 @@ def connect_llms():
         connected_llms[cfg["name"]] = llm
 
     _connected_llms = connected_llms
-    print(f"{len(_connected_llms)} LLMs connected successfully -", list(_connected_llms.keys()))
+    print(
+        f"{len(_connected_llms)} LLMs connected successfully -",
+        list(_connected_llms.keys()),
+    )
     return _connected_llms
 
 
+# def get_llm(name: str):
+#     llms = connect_llms()
+#     if name not in llms:
+#         raise ValueError(f"LLM '{name}' not found. Available: {list(llms.keys())}")
+#     return llms[name]
+
+
 def get_llm(name: str):
-    llms = connect_llms()
-    if name not in llms:
-        raise ValueError(f"LLM '{name}' not found. Available: {list(llms.keys())}")
-    return llms[name]
+    if name in _connected_llms:
+        return _connected_llms[name]
+
+    cfg = next((c for c in LLMs if c["name"] == name), None)
+    if not cfg:
+        raise ValueError(f"LLM '{name}' not found in LLMs.json")
+
+    hf_token = os.getenv("HF_TOKEN")
+    endpoint = HuggingFaceEndpoint(
+        task=cfg["task"],
+        repo_id=cfg["model_id"],
+        provider=cfg["provider"],
+        do_sample=cfg["do_sample"],
+        temperature=cfg["temperature"],
+        max_new_tokens=cfg["max_new_tokens"],
+        repetition_penalty=cfg["repetition_penalty"],
+        return_full_text=False,
+        huggingfacehub_api_token=hf_token,
+    )
+
+    llm = ChatHuggingFace(llm=endpoint) if cfg["task"] == "conversational" else endpoint
+    _connected_llms[name] = llm
+    print(f"Connected to LLM: {name}")
+    return llm
--- AI/main.py
+++ AI/main.py
@@ -4,15 +4,16 @@ from text_summarize import summarize_text
 from text_unverifiable import check_unverifiable
 
 if __name__ == "__main__":
-    llms = connect_llms()
+    # llms = connect_llms()
 
     statement1 = "John cena is my favourite wrestler"
     statement2 = "Sun is black"
     statement3 = "Brock Lesnar is best in the world"
     statement4 = "On 09/10/2025 had big accident in Pune between bus and truck"
     statement5 = "India got independence in 1905"
+    statement6 = "Brazil won 2022 cricket world cup"
 
     # print("\nSummarize:", summarize_text(statement))
     # print("\nUnverifiable:", check_unverifiable(statement3))
-    print("\nFact:", check_fact(statement4))
+    print("\nFact:", check_fact(statement6))
     print("\n")

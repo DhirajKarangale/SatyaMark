88479f490aaed1528405e8c6b1940cc8798dac28
Dhiraj Karangale <dakarangale02@gmail.com>
2026-01-15 12:32:58 +0530

    DB Protection bug fixed

--- Backend/src/model/db.js
+++ Backend/src/model/db.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT || 5432),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     ssl: process.env.DB_SSL === "true"
         ? { rejectUnauthorized: false }
         : false,
+    connectionTimeoutMillis: 10_000,
+});
+
+pool.on("error", err => {
+    console.log("PG Pool error:", err.message);
 });
 
 module.exports = pool;

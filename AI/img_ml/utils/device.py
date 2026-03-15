73a2a9dade0bcb407f2678cd2ff77a8f0876db23
Dhiraj Karangale <dakarangale02@gmail.com>
2026-03-15 20:41:41 +0530

    Old image verifycation removed

--- AI/img_ml/utils/device.py
+++
@@ -1,6 +0,0 @@
-# utils/device.py
-import torch
-def get_device(prefer_gpu=True):
-    if prefer_gpu and torch.cuda.is_available():
-        return torch.device("cuda")
-    return torch.device("cpu")
